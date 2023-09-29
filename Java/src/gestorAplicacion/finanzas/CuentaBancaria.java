0510142f2bba841c63c7cdd01c8305c95021b6f7
yohanCami <yosanchezm@unal.edu.co>
2023-09-28 20:34:23 -0500

    Creación de clases CuentaBancaria y Administrador

---
+++ Java/src/gestorAplicacion/finanzas/CuentaBancaria.java
@@ -0,0 +1,18 @@
+package gestorAplicacion.finanzas;
+
+public class CuentaBancaria {
+
+    private long numero;
+    private String banco;
+    private long saldo;
+    private Usuario titular;
+
+    public CuentaBancaria(){}
+
+    public void transferencia(Cuenta cuenta){}
+
+    public void depositar(long dinero){}
+
+    public void retirar(long dinero){}
+    
+}
---
+++ Java/src/gestorAplicacion/usuarios/Administrador.java
@@ -0,0 +1,30 @@
+package gestorAplicacion.usuarios;
+//from gestorAplicacion.hotel.Hotel import Hotel;
+
+public class Administrador {
+    //private Hotel hotel;
+    private int id;
+    //private static;
+
+    public Administrador(){}
+
+    public void crearAdministrador(){}
+
+    public void premiarUsuario(){}
+
+    public void hacerDescuento(){}
+
+    public void cobrarZonas(){}
+
+    public void agregarServicio(){}
+
+    public void alterarServicio(){}
+
+    public void calcularPromedio(){}
+
+    public void ultimoMesPago(){}
+
+    public void dineroCuentaHotel(){}
+
+    public void pagarEmpleado(){}
+}
