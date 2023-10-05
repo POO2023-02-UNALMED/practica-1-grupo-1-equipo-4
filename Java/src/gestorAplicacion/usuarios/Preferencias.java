31845e62740ef3d0f68fa87f949a1c92318ae2ec
juanRiveraAlvarez <juanpariveraalvarez@gmail.com>
2023-10-04 20:58:34 -0500

    Preferencias

--- Java/src/gestorAplicacion/usuarios/Preferencias.java
+++ Java/src/gestorAplicacion/usuarios/Preferencias.java
@@ -1,5 +1,42 @@
 package gestorAplicacion.usuarios;
 
-public enum Preferencias {
+public class Preferencias {
+    private String ciudad;
+    private String nombreHotel;
+    private String tipoHabitacion;
+    
+    public Preferencias(String ciudad, String nombreHotel, String tipoHabitacion) {
+        this.ciudad = ciudad;
+        this.nombreHotel = nombreHotel;
+        this.tipoHabitacion = tipoHabitacion;
+    }
 
+    public Preferencias() {
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getNombreHotel() {
+        return nombreHotel;
+    }
+
+    public void setNombreHotel(String nombreHotel) {
+        this.nombreHotel = nombreHotel;
+    }
+
+    public String getTipoHabitacion() {
+        return tipoHabitacion;
+    }
+
+    public void setTipoHabitacion(String tipoHabitacion) {
+        this.tipoHabitacion = tipoHabitacion;
+    }
+
+    
 }
